97789361fc307636b46c1c89dac89c64011da1ec
SDTP-Jhomme <dimasuayjhomme3@gmail.com>
2023-06-13 21:12:05 +0800

    13/06 9:11

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,8 @@
 
 <body>
 <div id="app" v-loading.fullscreen.lock="fullscreenLoading">
-        <section class="h-100 gradient-form" style="background-color: #eee;">
+@include('imports/nav')
+        <!-- <section class="h-100 gradient-form" style="background-color: #eee;">
             <div class="container py-5 h-100">
                 <div class="row d-flex justify-content-center align-items-center g-0 h-100">
                     <div class="col-xl-10">
@@ -65,7 +66,63 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
+    <section id="main" class="slide-in">
+        <div class="main-container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-lg-12 col-md-10 col-sm-12">
+                    <div class="row row-cols-2 align-items-center g-2">
+                        <div class="col-lg-6 col-md-12 col-sm-12 d-none d-sm-none d-lg-block left-fade-in">
+                            <h1 class="header-1">
+                                Learn From The Expert
+                            </h1>
+                            <h3 class="header-3">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet
+                                natus quas necessitatibus
+                            </h3>
+                        </div>
+                        <div class="col-lg-5 col-md-12 col-sm-12 col-12 right-fade-in">
+                            <div class="card" :visible.sync="studentLogin">
+                                <div class="card-body p-md-5 mx-md-4">
+                                    <div class="text-center">
+                                        <img src="<?php echo asset('assets/img/logo.png') ?>" style="width: 110px;" alt="logo">
+                                        <h4 class="mt-2 mb-4 pb-1">Student Login</h4>
+                                    </div>
+                                    <label class="form-label" for="identification">Identification No.</label>
+                                    <div class="form-outline">
+                                        <input v-on:keyup.enter="login" type="text" id="identification" class="form-control" :class="{'has-error': this.userErr}" v-model="identification" />
+                                    </div>
+                                    <div class="">
+                                        <span class="text-danger fst-italic error" v-text="userErr"></span>
+                                    </div>
+                                    <label class="form-label mt-4" for="password">Password</label>
+                                    <div class="form-outline input-group">
+                                        <input v-on:keyup.enter="login" :type="type" id="password" class="form-control" :class="{'has-error': this.passErr}" v-model="password" />
+                                        <button class="input-group-text" @click="showPassword" v-if="type == 'password'">
+                                            <span>
+                                                <i class="fa fa-eye"></i>
+                                            </span>
+                                        </button>
+                                        <button class="input-group-text" @click="hidePassword" v-if="type == 'text'">
+                                            <span>
+                                                <i class="fa fa-eye-slash"></i>
+                                            </span>
+                                        </button>
+                                    </div>
+                                    <div class="mb-5">
+                                        <span class="text-danger fst-italic error" v-text="passErr"></span>
+                                    </div>
+                                    <div class="text-center pt-1 mb-5 pb-1 d-grid gap-2">
+                                        <button @click="login" class="font-style py-2 btn btn-primary gradient-custom-2 text-uppercase btn-block fa-lg mb-3" type="submit">Login</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
         <footer class="py-4 bg-light mt-auto">
             <div class="container-fluid px-4">
                 <div class="d-flex align-items-center justify-content-between small">
