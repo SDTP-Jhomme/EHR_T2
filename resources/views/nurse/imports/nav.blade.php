3fa21d1afe7d6ca2bc765cb03be6ce0fb75f85cf
RjKira <adiksahentai0@gmail.com>
2023-06-28 05:30:29 -0700

    sdsd

--- resources/views/nurse/imports/nav.blade.php
+++ resources/views/nurse/imports/nav.blade.php
@@ -12,19 +12,13 @@
                 <div class="nav">
                     <li class="nav-item">
                         <div class="ms-auto mt-auto">
-                            <div v-for="avatarImg in fetchData" class="dropstart user-dropdown">
+                            <div class="dropstart user-dropdown">
                                 <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img class="img-xs rounded-circle" :src="avatarImg.avatar" alt="Profile image"> </a>
+                                    <img class="img-xs rounded-circle" src="<?php echo asset('assets/avatar/default.png') ?>" alt="Profile image"> </a>
                                 <div class="dropdown-menu" aria-labelledby="UserDropdown">
                                     <div class="dropdown-header text-center">
-                                        <img class="img-md rounded-circle" :src="avatarImg.avatar" alt="Profile image">
+                                        <img class="img-md rounded-circle" src="<?php echo asset('assets/avatar/default.png') ?>" alt="Profile image">
                                         <p class="mb-1 mt-3 fw-light text-muted">Nurse</p>
-                                    </div>
-                                    <div class="dropdown-item">
-                                        <a class="nav-link" href="#" @click="profile =true">
-                                            <i class="fas fa-user text-muted me-2"></i>
-                                            Profile
-                                        </a>
                                     </div>
                                     <div class="dropdown-item">
                                         <a class="nav-link" href="#" @click="logout">
